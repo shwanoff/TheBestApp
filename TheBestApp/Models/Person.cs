f070f65417544bf25a54802a3d59286c5a1aa5c0
Вадим Шванов <adm.shwan@gmail.com>
2018-07-01 21:40:37 +0300

    Добавьте файлы проекта.

---
+++ TheBestApp/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace TheBestApp.Models
+{
+    /// <summary>
+    /// Человек
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
